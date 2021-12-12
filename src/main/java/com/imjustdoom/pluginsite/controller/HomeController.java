38ced1d9098464fedea7f7025f19856dc4df6ac1
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-12-12 10:58:16 +1000

    DB stuff

--- src/main/java/com/imjustdoom/pluginsite/controller/HomeController.java
+++ src/main/java/com/imjustdoom/pluginsite/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.imjustdoom.pluginsite.controller;
 
 import com.imjustdoom.pluginsite.model.Account;
+import com.imjustdoom.pluginsite.repositories.BlogRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@AllArgsConstructor
 public class HomeController {
+
+    private final BlogRepository blogRepository;
 
     @GetMapping("/")
     public String home(Model model, Authentication authentication) {
+
+        blogRepository.findAll();
+
         model.addAttribute("auth", authentication);
         return "home";
     }
---
+++ src/main/java/com/imjustdoom/pluginsite/model/Blog.java
@@ -0,0 +1,30 @@
+package com.imjustdoom.pluginsite.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+public class Blog {
+
+    public Blog(String post) {
+        this.post = post;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String post;
+}
