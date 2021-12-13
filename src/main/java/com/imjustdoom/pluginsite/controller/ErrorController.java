d8a067f49cb200bdbdc1306745dc456a711bc982
cyric2020 <Jordyboy.grieve@gmail.com>
2021-12-13 20:41:37 +1000

    added proper 404 handling

---
+++ src/main/java/com/imjustdoom/pluginsite/controller/ErrorController.java
@@ -0,0 +1,19 @@
+package com.imjustdoom.pluginsite.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ErrorController {
+    /* idk what an Exception error is */
+    @ExceptionHandler(Exception.class)
+    public String handleException() {
+//        return "error/error";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String handle404() {
+        return "error/404";
+    }
+}
