bc418785d75e4b6b77f39adcbd861305f863b07b
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-12-14 11:17:53 +1000

    Update ExternalUrlController.java

--- src/main/java/com/imjustdoom/pluginsite/controller/ExternalUrlController.java
+++ src/main/java/com/imjustdoom/pluginsite/controller/ExternalUrlController.java
@@ -1,6 +1,8 @@
 package com.imjustdoom.pluginsite.controller;
 
+import com.imjustdoom.pluginsite.model.Account;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class ExternalUrlController {
     @GetMapping("/redirect")
-    public String redirect(HttpServletResponse response, @RequestParam(name = "url", required = false, value = "") String url){
+    public String redirect(Model model, HttpServletResponse response, @RequestParam(name = "url", required = false, value = "") String url, Account account){
         // do you know how to remove the url from the parameters?
 
         url.replaceAll("%2F", "/");
@@ -18,6 +20,8 @@ public class ExternalUrlController {
         Cookie cookie = new Cookie("url", url);
 
         response.addCookie(cookie);
+
+        model.addAttribute("account", account);
 
         return "redirect/redirect";
     }
