0e10c73551f12278bccb6a0a370fd81c18e9e5d2
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-12-14 12:06:54 +1000

    Set role admin page

--- src/main/java/com/imjustdoom/pluginsite/repositories/AccountRepository.java
+++ src/main/java/com/imjustdoom/pluginsite/repositories/AccountRepository.java
@@ -2,8 +2,11 @@ package com.imjustdoom.pluginsite.repositories;
 
 import com.imjustdoom.pluginsite.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +18,9 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     boolean existsByEmailEqualsIgnoreCase(String email);
 
     Optional<Account> findByUsernameEqualsIgnoreCase(String username);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Account account SET account.role = ?2 WHERE account.id = ?1")
+    void setRoleById(int id, String role);
 }
