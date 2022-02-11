1e040dcbebc29eb7731c98c60d507b4db8ac10c7
Zak <shearmanzak@gmail.com>
2022-02-11 06:55:57 +0000

    Clean up date time formatter

--- src/main/java/com/imjustdoom/pluginsite/util/DateUtil.java
+++ src/main/java/com/imjustdoom/pluginsite/util/DateUtil.java
@@ -3,10 +3,9 @@ package com.imjustdoom.pluginsite.util;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
-
-    public static DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ssa");
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ssa");
 
     public static DateTimeFormatter getDateFormatter() {
-        return format;
+        return FORMAT;
     }
 }
